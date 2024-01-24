b080f52cdbda80ae97c62c453ebe2286391824c5
Eduardo Rigo <eduardorigom@gmail.com>
2024-01-24 20:36:38 -0300

    Primeiro Commit

---
+++ components/Camp.tsx
@@ -0,0 +1,84 @@
+import { PEOPLE_URL } from "@/constants";
+import Image from "next/image";
+
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
+}
+
+const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps) => {
+ return(
+  <div className={`h-full w-full lg:min-w-[1100px] md:min-w-[800px] min-w-[400px] ${backgroundImage} bg-cover bg-no-repeat rounded-3xl lg:rounded-r-5xl 2xl:rounded-5xl`}>
+    <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
+      <div className="flexCenter gap-4">
+        <div className="rounded-full bg-green-50 p-4">
+          <Image 
+            src="/folded-map.svg"
+            alt="map"
+            width={28}
+            height={28}
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <h4 className="bold-18 text-white">{title}</h4>
+          <p className="regular-14 text-white">{subtitle}</p>
+        </div>
+      </div>
+      <div className="flexCenter gap-6">
+        <span className="flex -space-x-4 overflow-hidden">
+          {PEOPLE_URL.map((url)=>(
+            <Image
+              className="inline-block h-10 w-10 rounded-full" 
+              src={url}
+              key={url}
+              alt="person"
+              width={52}
+              height={52}
+            />
+          ))}
+        </span>
+        <p className="bold-16 md:bold-20 text-white">{peopleJoined}</p>
+      </div>
+    </div>
+  </div>
+ )
+}
+
+const Camp = () => {
+  return (
+   <section className=" 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+    <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+      <CampSite 
+        backgroundImage="bg-bg-img-1"
+        title="Acampamento Putuk Truno "
+        subtitle="Prigen, Pasuruan"
+        peopleJoined="+50 participantes"
+      />
+      <CampSite 
+        backgroundImage="bg-bg-img-2"
+        title="Acampamento na Montanha"
+        subtitle="Em algum lugar da floresta"
+        peopleJoined="+17 participantes"
+      />
+    </div>
+
+    <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
+      <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5-xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
+        <h2 className="regular-24 md:regular-32 2xl:regular-64 capitalize text-white"><strong>Se Sentindo Perdido </strong> e Sem Saber o Caminho?</h2>
+        <p className="regular-14 xl:regular-16 mt-5 text-white">Partindo da ansiedade dos escaladores ao visitar uma nova localidade de escalada, a possibilidade de se perder é bastante grande. É por isso que estamos aqui para aqueles que desejam iniciar uma aventura.</p>
+        <Image 
+          src="/quote.svg"
+          alt="camp-2"
+          width={186}
+          height={219}
+          className="camp-quote"
+        />
+      </div>
+    </div>
+   </section>
+  )
+}
+
+export default Camp
---
+++ constants/index.ts
@@ -0,0 +1,83 @@
+// NAVIGATION
+export const NAV_LINKS = [
+  { href: '/', key: 'home', label: 'Home' },
+  { href: '/', key: 'how_hilink_work', label: 'How Hilink Work?' },
+  { href: '/', key: 'services', label: 'Services' },
+  { href: '/', key: 'pricing ', label: 'Pricing ' },
+  { href: '/', key: 'contact_us', label: 'Contact Us' },
+];
+
+// CAMP SECTION
+export const PEOPLE_URL = [
+  '/person-1.png',
+  '/person-2.png',
+  '/person-3.png',
+  '/person-4.png',
+];
+
+// FEATURES SECTION
+export const FEATURES = [
+  {
+    title: 'Mapas reais podem ser usados offline',
+    icon: '/map.svg',
+    variant: 'verde',
+    description: 'Oferecemos uma solução para que você possa usar nosso aplicativo ao escalar, sim, mapas offline que você pode usar a qualquer momento, mesmo sem sinal no local.',
+  },
+  {
+    title: 'Defina um cronograma de aventuras',
+    icon: '/calendar.svg',
+    variant: 'verde',
+    description: 'Agende uma aventura com amigos. Nos feriados, há muitas ofertas interessantes da Hilink. Dessa forma, não há mais discussão.',
+  },
+  {
+    title: 'Tecnologia utilizando realidade aumentada',
+    icon: '/tech.svg',
+    variant: 'verde',
+    description: 'A tecnologia utiliza realidade aumentada como guia para sua trilha de caminhada na floresta até o topo da montanha. Já suportado pela tecnologia mais recente sem necessidade de conexão com a internet.',
+  },
+  {
+    title: 'Novas localidades todo mês',
+    icon: '/location.svg',
+    variant: 'laranja',
+    description: 'Novas localidades todo mês, porque temos uma comunidade mundial de escaladores que compartilham suas melhores experiências com a escalada.',
+  },
+];
+
+// FOOTER SECTION
+export const FOOTER_LINKS = [
+
+  {
+    title: 'Saiba Mais',
+    links: [
+      'Sobre a Hilink',
+      'Comunicados de Imprensa',
+      'Meio Ambiente',
+      'Vagas de Emprego',
+      'Política de Privacidade',
+      'Contate-nos',
+    ],
+  },
+  {
+    title: 'Nossa Comunidade',
+    links: ['Escalada hilink', 'Caminhada hilink', 'Kinthill Hilink'],
+  }
+];
+
+export const FOOTER_CONTACT_INFO = {
+  title: 'Entre em Contato',
+  links: [
+    { label: 'Administrativo', value: '123-456-7890' },
+    { label: 'Email', value: 'hilink@akinthil.com' },
+  ],
+};
+
+export const SOCIALS = {
+  title: 'Redes Sociais',
+  links: [
+    '/facebook.svg',
+    '/instagram.svg',
+    '/twitter.svg',
+    '/youtube.svg',
+    '/wordpress.svg',
+  ],
+};
